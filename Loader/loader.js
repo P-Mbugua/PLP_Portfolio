1fe945e753799590a68ecbc3b83110dcc6ce9add
Peter <pmbugua276@gmail.com>
2025-04-03 20:44:23 +0300

    added all the files

---
+++ Loader/loader.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulate loading time
+    setTimeout(() => {
+        document.querySelector(".loader-container").style.display = "none";
+    }, 2000); // Adjust time as needed
+});
