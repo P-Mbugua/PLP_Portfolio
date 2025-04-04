5caf6b11666f3a404a0c996700451ba7753ef000
Peter <pmbugua276@gmail.com>
2025-04-04 23:50:23 +0300

    removed all the unnecessary folders and cleared everything

--- Loader/loader.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Simulate loading time
-    setTimeout(() => {
-        document.querySelector(".loader-container").style.display = "none";
-    }, 2000); // Adjust time as needed
-});
